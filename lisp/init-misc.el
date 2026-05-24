4f2dce9a7e43c9216470e1f53cc95c06c2ccd548
Wang Ding <ggwdwhu@gmail.com>
2026-05-24 16:29:22 +0800

    update

--- lisp/init-misc.el
+++ lisp/init-misc.el
@@ -4,5 +4,8 @@
 (require 'init-tools-misc)
 (require 'init-core-runtime)
 
+(if-let (file-org-capture "~/projects/2026/haskell-web/scripts/elisp/lib-org-capture.el")
+  (load file-org-capture))
+
 (provide 'init-misc)
 ;;; init-misc.el ends here
