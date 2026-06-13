b395017cb56fb2515cc19c655ae184965bdbaa89
Wang Ding <ggwdwhu@gmail.com>
2026-06-13 18:19:18 +0800

    no doom-ui anymore

--- lisp/init-ui-misc.el
+++ lisp/init-ui-misc.el
@@ -2,15 +2,11 @@
 
 (setq! +workspaces-data-file (concat (system-name) "_workspaces"))
 
-(use-package! doom-ui
-  :init
-  (setq initial-scratch-message ";; Happy hacking,  - Emacs loves Melt!\12\12")
-  :custom
-  (initial-scratch-message (concat ";; Happy hacking, " user-full-name " - Emacs ♥ you!\n\n"))
-  (fancy-splash-image (file-truename (concat doom-user-dir "assets/2025/bitmap_resized_2.png")))
-  (imenu-auto-rescan t)
-  (user-full-name "Wang Ding")
-  (user-mail-address "ggwdwhu@gmail.com"))
+(setq user-full-name "Wang Ding"
+      user-mail-address "ggwdwhu@gmail.com"
+      initial-scratch-message (concat ";; Happy hacking, " user-full-name " - Emacs ♥ you!\n\n")
+      fancy-splash-image (file-truename (concat doom-user-dir "assets/2025/bitmap_resized_2.png"))
+      imenu-auto-rescan t)
 
 (provide 'init-ui-misc)
 ;;; init-ui-misc.el ends here
