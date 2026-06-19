a707f825105a678bfea31781672ed47a5c89c9ba
Wang Ding <ggwdwhu@gmail.com>
2026-06-19 12:59:33 +0800

    update

--- lisp/init-term.el
+++ lisp/init-term.el
@@ -4,7 +4,6 @@
   (:option
    ;; Prefer vendored libvterm to avoid depending on system curses/ncurses headers.
    vterm-module-cmake-args "-DUSE_SYSTEM_LIBVTERM=Off"
-   vterm-shell (or (executable-find "zsh") (executable-find "bash"))
    vterm-tramp-shells '(("sshx" login-shell "/bin/zsh" "/bin/bash")
                         ("ssh" login-shell "/bin/zsh" "/bin/bash")
                         ("scp" login-shell "/bin/zsh" "/bin/bash")
