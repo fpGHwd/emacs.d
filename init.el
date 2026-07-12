fe29aee6ee743d3a6ae80bc26c4f21d99dbf43c8
Wang Ding <ggwdwhu@gmail.com>
2026-07-12 08:54:56 +0800

    remove flymake

--- init.el
+++ init.el
@@ -85,7 +85,7 @@
        vterm               ; the best terminal emulation in Emacs
 
        :checkers
-       (syntax +childframe +flymake +icons) ; tasing you for every semicolon you forget
+       (syntax +childframe +icons) ; tasing you for every semicolon you forget
        ;;(spell +flyspell) ; tasing you for misspelling mispelling
        ;;grammar           ; tasing grammar mistake every you make
 
