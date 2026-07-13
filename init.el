413e793e8725f2b5251b4d6564ebbf5be34ea919
Wang Ding <ggwdwhu@gmail.com>
2026-07-13 21:02:42 +0800

    clean init.el

--- init.el
+++ init.el
@@ -41,22 +41,22 @@
        modeline            ; snazzy, Atom-inspired modeline, plus API
        ;;nav-flash         ; blink cursor line after big motions
        ;;neotree           ; a project drawer, like NERDTree for vim
-       ophints             ; highlight the region an operation acts on
+       ;; ophints             ; highlight the region an operation acts on
        (popup +defaults)   ; tame sudden yet inevitable temporary windows
        ;;smooth-scroll     ; So smooth you won't believe it's not butter
        ;;tabs              ; a tab bar for Emacs
        ;;treemacs          ; a project drawer, like neotree but cooler
        ;;unicode           ; extended unicode support for various languages
        (vc-gutter +pretty) ; vcs diff in the fringe
-       vi-tilde-fringe     ; fringe tildes to mark beyond EOB
-       (window-select +numbers) ; visually switch windows
+       ;; vi-tilde-fringe     ; fringe tildes to mark beyond EOB
+       ;; (window-select +numbers) ; visually switch windows
        workspaces          ; tab emulation, persistence & separate workspaces
-       zen                 ; distraction-free coding or writing
+       ;; zen                 ; distraction-free coding or writing
 
        :editor
        ;;(evil +everywhere) ; come to the dark side, we have cookies
        file-templates      ; auto-snippets for empty files
-       fold                ; (nigh) universal code folding
+       ;; fold                ; (nigh) universal code folding
        ;;(format +onsave)  ; automated prettiness
        ;;god               ; run Emacs commands without modifier keys
        lispy               ; vim for lisp, for people who don't like vim
@@ -65,21 +65,21 @@
        ;;parinfer          ; turn lisp into python, sort of
        ;;rotate-text       ; cycle region at point between text candidates
        snippets            ; my elves. They type so I don't have to
-       (whitespace +guess +trim) ; a butler for your whitespace
+       ;; (whitespace +guess +trim) ; a butler for your whitespace
        word-wrap           ; soft wrapping with language-aware indent
        (meow +qwerty +tree-sitter)
 
        :emacs
        (dired +dirvish +icons) ; making dired pretty [functional]
        ;; electric         ; smarter, keyword-based electric-indent
-       eww                 ; the internet is gross
+       ;; eww                 ; the internet is gross
        ;;ibuffer           ; interactive buffer management
        tramp               ; remote files at your arthritic fingertips
        undo                ; persistent, smarter undo for your inevitable mistakes
        vc                  ; version-control and Emacs, sitting in a tree
 
        :term
-       eshell              ; the elisp shell that works everywhere
+       ;; eshell              ; the elisp shell that works everywhere
        ;;shell             ; simple shell REPL for Emacs
        ;;term              ; basic terminal emulator for Emacs
        vterm               ; the best terminal emulation in Emacs
@@ -120,8 +120,8 @@
        ;;agda              ; types of types of types of types...
        ;;beancount         ; mind the GAAP
        (cc +lsp +tree-sitter)           ; C > C++ == 1
-       (clojure +tree-sitter +lsp)      ; java with a lisp
-       common-lisp       ; if you've seen one lisp, you've seen them all
+       ;; (clojure +tree-sitter +lsp)      ; java with a lisp
+       ;; common-lisp       ; if you've seen one lisp, you've seen them all
        ;;coq               ; proofs-as-programs
        ;;crystal           ; ruby at the speed of c
        ;;csharp            ; unity, .NET, and mono shenanigans
@@ -150,7 +150,7 @@
        ;;(javascript +lsp +tree-sitter) ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
-       (latex +fold +lsp)  ; writing papers in Emacs has never been so fun
+       ;; (latex +fold +lsp)  ; writing papers in Emacs has never been so fun
        ;;lean              ; for folks with too much to prove
        ledger              ; be audit you can be
        ;;lua               ; one-based indices? one-based indices
@@ -159,11 +159,12 @@
        (nix +tree-sitter +lsp)          ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
        ;;odin              ; C, minus its footguns
-       (org +brain +contacts +crypt +gnuplot +journal +dragndrop
-            +pandoc +noter +roam +pretty +org-tree-slide +present
-            +pomodoro)         ; organize your plain life in plain text
+       (org +crypt +gnuplot
+            +journal +dragndrop
+            +pandoc +noter
+            +roam +pretty) ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
-       plantuml            ; diagrams for confusing people more
+       ;; plantuml            ; diagrams for confusing people more
        ;;graphviz          ; diagrams for confusing yourself even more
        ;;purescript        ; javascript, but functional
        (python +lsp +poetry +tree-sitter)
@@ -175,14 +176,14 @@
        ;;(ruby +rails +tree-sitter)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
        ;;(rust +tree-sitter)            ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala             ; java, but good
-       (scheme +guile)   ; a fully conniving family of lisps
+       ;; (scheme +guile)   ; a fully conniving family of lisps
        (sh +tree-sitter) ; she sells {ba,z,fi}sh shells on the C xor
        ;;sml
        ;;solidity          ; do you need a blockchain? No.
        ;;swift             ; who asked for emoji variables?
        ;;terra             ; Earth and Moon in alignment for performance.
        ;;(web +lsp +tree-sitter)        ; the tubes
-       yaml                ; JSON, but readable
+       ;; yaml                ; JSON, but readable
        ;;zig               ; C, but simpler
 
        :email
