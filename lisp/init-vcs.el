4bf53d302816d71cf2a318c10120d033c103bbca
wangding02 <wangding02@saicmotor.com>
2026-07-13 14:17:32 +0800

    fix git-commit insert mode: use hook instead of meow-mode-state-list
    git-commit-mode is a minor mode, meow matches on major mode (text-mode),
    so the meow-mode-state-list entry was ineffective. Use git-commit-setup-hook
    with meow-insert-mode instead.

--- lisp/init-vcs.el
+++ lisp/init-vcs.el
@@ -8,7 +8,9 @@
   (:also-load lib-git)
   (:when-loaded
    (with-eval-after-load 'meow
-     (add-to-list 'meow-mode-state-list '(git-commit-mode . insert)))))
+     ;; git-commit-mode is a minor mode, meow matches on major mode (text-mode).
+     ;; Use git-commit-setup-hook to switch to insert state instead.
+     (add-hook 'git-commit-setup-hook #'meow-insert-mode))))
 
 (provide 'init-vcs)
 ;;; init-vcs.el ends here
