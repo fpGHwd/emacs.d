e32758b23d432864a3481e9b5ed1da1a902386e0
Wang Ding <ggwdwhu@gmail.com>
2026-07-27 19:56:19 +0800

    refactor

--- lisp/init-editor.el
+++ lisp/init-editor.el
@@ -2,6 +2,13 @@
 
 (setup meow
   (:also-load lib-util)
+  (:hooks doom-after-reload-hook
+          (lambda ()
+            (setq meow-cursor-type-normal 'box
+                  meow-cursor-type-motion 'box
+                  meow-cursor-type-beacon 'box
+                  meow-cursor-type-insert 'bar
+                  blink-cursor-interval 0.618)))
   (:when-loaded
     (meow-normal-define-key
      '("RET" . +wd/meow-normal-return)
@@ -22,15 +29,6 @@
                   (when-let ((buf (get-buffer "*Meow Cheatsheet*")))
                     (with-current-buffer buf
                       (buffer-face-set :family "Sarasa Fixed SC")))))))
-
-;; Doom module :config re-executes on doom/reload, overriding the :option values
-;; above. Re-apply after reload to ensure our cursor types win.
-(add-hook! 'doom-after-reload-hook
-  (setq meow-cursor-type-normal 'box
-        meow-cursor-type-motion 'box
-        meow-cursor-type-beacon 'box
-        meow-cursor-type-insert 'bar
-        blink-cursor-interval 0.618))
 
 (provide 'init-editor)
 ;;; init-editor.el ends here
