31739ff8758692cdfa98ffbaea25a001f6a2f288
Wang Ding <ggwdwhu@gmail.com>
2026-08-09 16:37:01 +0800

    Keep Linux fullscreen behavior

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -4,28 +4,11 @@
   (set-face-attribute 'font-lock-comment-face t :slant 'italic)
   (set-face-attribute 'font-lock-keyword-face t :slant 'italic))
 
-(setq default-frame-alist (assq-delete-all 'fullscreen default-frame-alist))
-
-(defun +wd/macos-native-fullscreen-frame (frame)
-  "Enter macOS native fullscreen for FRAME after it is visible."
-  (when (and (eq system-type 'darwin)
-             (display-graphic-p frame))
-    (run-at-time
-     "0.5 sec" nil
-     (lambda (frame)
-       (when (frame-live-p frame)
-         (with-selected-frame frame
-           (unless (eq (frame-parameter frame 'fullscreen) 'fullscreen)
-             (call-interactively
-              (if (fboundp 'mac-toggle-frame-fullscreen)
-                  #'mac-toggle-frame-fullscreen
-                #'toggle-frame-fullscreen))))))
-     frame)))
-
-(when (eq system-type 'darwin)
-  (add-hook 'emacs-startup-hook
-            (lambda () (+wd/macos-native-fullscreen-frame (selected-frame))))
-  (add-hook 'after-make-frame-functions #'+wd/macos-native-fullscreen-frame))
+(if (eq system-type 'darwin)
+    (add-hook 'emacs-startup-hook
+              (lambda ()
+                (run-at-time "0.5 sec" nil #'mac-toggle-frame-fullscreen)))
+  (add-to-list 'default-frame-alist '(fullscreen . fullboth)))
 
 (setq imenu-auto-rescan t)
 
