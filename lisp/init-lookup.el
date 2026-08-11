5e792b54b20e690a0ae86ae10f00b44ad1fd7f6c
Wang Ding <ggwdwhu@gmail.com>
2026-08-11 19:32:24 +0800

    fix

--- lisp/init-lookup.el
+++ lisp/init-lookup.el
@@ -3,13 +3,8 @@
 (setup doom
   (:when-loaded
     (map! :leader
-          (:prefix ("z" . "Wang Ding defining"))
-          :desc "Reading via Calibre" "r" #'calibredb)
-    (when (string= (system-name) "ubuntu2204")
-      (add-to-list '+lookup-provider-url-alist
-                   '("Bing" "https://cn.bing.com/search?go=Search&q=%s&qs=ds&form=QBRE")))
-    (add-to-list '+lookup-provider-url-alist
-                 '("NixOS Package Search" "https://search.nixos.org/packages?channel=25.11&query=%s"))))
+          (:prefix ("z" . "Wang Ding defining")
+           :desc "Reading via Calibre" "r" #'calibredb))))
 
 (provide 'init-lookup)
 ;;; init-lookup.el ends here
