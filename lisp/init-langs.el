d64da39d73370f9ebd97fc0b982d16b8ce24189d
wangding02 <wangding02@saicmotor.com>
2026-08-25 16:50:38 +0800

    add Kbuild to makefile-mode and remove useless mode

--- lisp/init-langs.el
+++ lisp/init-langs.el
@@ -4,14 +4,10 @@
 ;; producing false "free variable" warnings for config files. Just it out.
 (add-hook! 'emacs-lisp-mode-hook (flycheck-mode -1))
 
+(add-to-list 'auto-mode-alist '("Kbuild\\'" . makefile-mode))
+
 (setup eglot
   (:option eglot-max-file-watches 524288))
-
-(after! treesit
-  (setf (alist-get 'go treesit-language-source-alist)
-        '("https://github.com/tree-sitter/tree-sitter-go" "v0.23.4" nil nil nil nil))
-  (setf (alist-get 'xml treesit-language-source-alist)
-        '("https://github.com/tree-sitter/tree-sitter-xml" nil "grammars/xml/src" nil nil nil)))
 
 (setup sql
   (:option sql-mysql-program "mariadb"))
