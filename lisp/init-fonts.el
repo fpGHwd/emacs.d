72714b6b74666a336dc193333db3d8ae6aa56035
wangding02 <wangding02@saicmotor.com>
2026-09-03 09:09:00 +0800

    tune font size

--- lisp/init-fonts.el
+++ lisp/init-fonts.el
@@ -3,7 +3,7 @@
 (defconst +wd/code-font "Sarasa Fixed SC")
 (defconst +wd/cjk-font "Sarasa Gothic SC")
 (defconst +wd/fixed-cjk-font "Sarasa Fixed SC")
-(defconst +wd/font-size (if *is-work* 18 15))
+(defconst +wd/font-size (if *is-work* 18 16))
 
 (unless *is-home*
   (dolist (family (list +wd/code-font +wd/cjk-font +wd/fixed-cjk-font))
