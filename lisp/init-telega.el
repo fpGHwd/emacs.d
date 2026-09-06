8c840041dfc22d11a31a121b028ac1d67d0e779f
Wang Ding <ggwdwhu@gmail.com>
2026-09-06 13:40:08 +0800

    fix telega start when home

--- lisp/init-telega.el
+++ lisp/init-telega.el
@@ -127,27 +127,22 @@
 
 ;; https://github.com/zevlg/telega.el
 (setup telega
+  (when *is-home*
+    ;; defer before when-loaded, then loaded to configure other options
+    (:defer (telega t))) 
   (:when-loaded
+    (:setopt
+     telega-cache-dir (file-truename "~/.config/telega/cache")
+     telega-directory (file-truename "~/.config/telega/")
+     telega-server-logfile (file-truename "~/.config/telega/telega-server.log")
+     telega-temp-dir (file-truename "~/.config/telega/temp")
+     telega-database-dir (file-truename "~/.config/telega/")
+     telega-server-libs-prefix (file-truename "~/.nix-profile/"))
     (when *is-home*
-      (:defer (telega t))
-      (:setopt
-       telega-cache-dir (file-truename "~/.config/telega/cache")
-       telega-directory (file-truename "~/.config/telega/")
-       telega-server-logfile (file-truename "~/.config/telega/telega-server.log")
-       telega-temp-dir (file-truename "~/.config/telega/temp")
-       telega-database-dir (file-truename "~/.config/telega/")
-       telega-server-libs-prefix (file-truename "~/.nix-profile/"))
       (setq +wd/telegram-cmb-chat-id
-               (password-store-get "telegram/TELEGRAM_CMB_CHAT_ID"))
+            (password-store-get "telegram/TELEGRAM_CMB_CHAT_ID"))
       (:hooks telega-chat-update-hook +wd/telega-chat-update-function
-              telega-chat-mode-hook (lambda () (company-mode -1)))
-      ;; telea font
-      (when (member "Sarasa Mono SC" (font-family-list))
-        (make-face 'telega-align-by-sarasa)
-        (set-face-font 'telega-align-by-sarasa (font-spec :family "Sarasa Mono SC"))
-        (:hooks
-         telega-chat-mode-hook (lambda () (buffer-face-set 'telega-align-by-sarasa))
-         telega-root-mode-hook (lambda () (buffer-face-set 'telega-align-by-sarasa)))))))
+              telega-chat-mode-hook (lambda () (company-mode -1))))))
 
 (provide 'init-telega)
 ;;; init-telega.el ends here
