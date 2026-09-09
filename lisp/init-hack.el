d779e660ec5a0815570b8f256a10ec73b60bcfcf
wangding02 <wangding02@saicmotor.com>
2026-09-09 11:48:04 +0800

    customize elf-mode-command

--- lisp/init-hack.el
+++ lisp/init-hack.el
@@ -3,7 +3,9 @@
 (setup elf-mode
   ;; Match ELF files by magic number, not just by extension
   ;; Note: \x7f does NOT work in Elisp strings; use octal \177 instead
-  (add-to-list 'magic-mode-alist '("\177ELF" . elf-mode)))
+  (add-to-list 'magic-mode-alist '("\177ELF" . elf-mode))
+  (:when-loaded
+    (:setopt elf-mode-command "readelf -a -W %s")))
 
 (setup demangle-mode
   ;; Auto-enable in compilation buffers and shell output with symbols
